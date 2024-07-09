6ccf0c0a56cd3048db85964910b552b637131e40
rabiaconline <rabiah.saleem@cabonline.com>
2024-07-09 14:55:23 +0200

    updated resource

--- src/main/java/com/spring/mongo/resources/DocumentResource.java
+++ src/main/java/com/spring/mongo/resources/DocumentResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/hello")
 
 public class DocumentResource {
     private MongoService mongoService;
